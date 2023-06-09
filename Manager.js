d31faf8eb99c00821ddce0a14860a2a6ecba8023
Brando perez <brandoperezinciarte@gmail.com>
2023-06-08 21:33:21 -0300

    entrega-tres

---
+++ Manager.js
@@ -0,0 +1,59 @@
+class PproductManager{
+    constructor(){
+        this.products = []
+    }
+
+    getProducts = () =>{
+        return this.products
+    }
+
+    addProduct=(title, description, precio, thumbnail, code, stock=25)=>{
+        const producto = {
+            title,
+            description,
+            precio, 
+            thumbnail,
+            code,
+            stock
+        } 
+
+        let id = 0;
+
+        if(this.products.length === 0){
+            producto.id = 1;
+        }
+        else{
+            id = this.products.slice(-1)[0].id+1
+           // producto.id = this.products[this.products.length -1].id+1
+        }
+            this.products.push({
+                ...producto, id
+                //id:this.products[this.products.length-1]+1
+            });
+        
+        for(let i = 0; i < this.products.length; i++){
+                        console.log(this.products[i]);
+                }
+        
+        
+        
+    }
+    getProductById=()=>{
+        if (!this.products.find((producto) => producto.id === id)){
+            console.log("El producto no pudo ser encontrado");
+        }else{
+            console.log(this.products.find((producto) => producto.id === id)) 
+        }
+    }
+
+}
+
+
+
+creadorProductos = new ProductManager();
+
+creadorProductos.addProduct("NuevoProducto1", "Este es un producto de prueba 1", 5000, "Sin imagen", "123def", 7);
+creadorProductos.addProduct("NuevoProducto2", "Este es un producto de prueba 2", 3000, "Sin imagen", "456ghj", 2);
+creadorProductos.addProduct("NuevoProducto2", "Este es un producto de prueba 2", 3000, "Sin imagen", "456ghj", 2);
+
+
--- productManager.js
+++ productManager.js
@@ -1,43 +1,41 @@
-class ProductManager{
+import fs from "fs";
+
+const path = '../archivoProductos/productos.json';
+
+export default class ProductManager{
     constructor(){
         this.products = []
     }
 
-    getProducts = () =>{
-        return this.products
-    }
-
-    addProduct=(title, description, precio, thumbnail, code, stock=25)=>{
-        const producto = {
-            title,
-            description,
-            precio, 
-            thumbnail,
-            code,
-            stock
-        } 
-
-        let id = 0;
-
-        if(this.products.length === 0){
-            producto.id = 1;
+    getProducts = async () =>{
+        if(fs.existsSync(path)){
+            const datosProductos = await fs.promises.readFile(path, "utf-8")
+            console.log(datosProductos);
+            const productos = JSON.parse(datosProductos)
+            return productos
+        }else{
+            return[]
         }
-        else{
-            id = this.products.slice(-1)[0].id+1
-           // producto.id = this.products[this.products.length -1].id+1
-        }
-            this.products.push({
-                ...producto, id
-                //id:this.products[this.products.length-1]+1
-            });
+    } 
+
+
+
+   /* addProduct= async (producto)=>{
+        const producto = await this.getProducts();
+            if(producto.length === 0){
+                producto.id = 1
+            }else{
+                producto.id = producto[producto.length-1].id+1;
+            }
+
+                producto.push(producto)
+                await fs.promises.writeFile(this.path, JSON.stringify(productos, null, '/T'))
+                return producto
+
+       
+
         
-        for(let i = 0; i < this.products.length; i++){
-                        console.log(this.products[i]);
-                }
-        
-        
-        
-    }
+    }*/
     getProductById=()=>{
         if (!this.products.find((producto) => producto.id === id)){
             console.log("El producto no pudo ser encontrado");
@@ -47,13 +45,3 @@ class ProductManager{
     }
 
 }
-
-
-
-creadorProductos = new ProductManager();
-
-creadorProductos.addProduct("NuevoProducto1", "Este es un producto de prueba 1", 5000, "Sin imagen", "123def", 7);
-creadorProductos.addProduct("NuevoProducto2", "Este es un producto de prueba 2", 3000, "Sin imagen", "456ghj", 2);
-creadorProductos.addProduct("NuevoProducto2", "Este es un producto de prueba 2", 3000, "Sin imagen", "456ghj", 2);
-
-
